89df7390d97faecc8f4aef6141d6eedb4bf72182
gagan <gasingla07@gmail.com>
2026-02-13 15:25:48 -0600

    add variable rules

--- ch1/hello.go
+++ ch1/hello.go
@@ -3,30 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world")
-	fmt.Printf("Hello, %s!\n", "world")
-
-	// var x int = 10
-
-	// x *= 2
-
-	// fmt.Println(x)
-
-	var myFirstinitial rune = 'g'
-
-	fmt.Printf("%c\n",myFirstinitial)
-
-	var x int = 10
-	var y float64 = 30.2
-	var sum1 float64 = float64(x) + y
-	var sum2 int = x + int(y)
-	fmt.Println(sum1,sum2)
-
-	var b int= 100
-	b+= (900)
-	fmt.Println(b)
-
-	var num, str = 10, "hello"
-	fmt.Println(num)
-	fmt.Println(str)
+	x,y := 3,4
+	fmt.Println(x)
+	fmt.Println(y)
 }
