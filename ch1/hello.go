b3bdb655d8e11c934f4d892d541cf06fcc7423fe
gagan <gasingla07@gmail.com>
2026-02-13 14:16:51 -0600

    predeclared types

--- ch1/hello.go
+++ ch1/hello.go
@@ -5,4 +5,20 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, world")
 	fmt.Printf("Hello, %s!\n", "world")
+
+	// var x int = 10
+
+	// x *= 2
+
+	// fmt.Println(x)
+
+	var myFirstinitial rune = 'g'
+
+	fmt.Printf("%c\n",myFirstinitial)
+
+	var x int = 10
+	var y float64 = 30.2
+	var sum1 float64 = float64(x) + y
+	var sum2 int = x + int(y)
+	fmt.Println(sum1,sum2)
 }
