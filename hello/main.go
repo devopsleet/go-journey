859ce527df0ae89d4f52eded855387c2a62ef4c5
gagan <gasingla07@gmail.com>
2026-02-19 13:52:53 -0600

    add hello folder

---
+++ hello/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt" 
+
+var flag bool
+var isAwesome = true
+
+func main() {
+
+
+	fmt.Println("hello from go")
+
+	fmt.Println("Hello from golang")
+
+	fmt.Println("Greetings and \"Salutations\"")
+
+	// fmt.Println(2.3/0.0)
+
+	var x float64 = -5.0
+	var y float64 = 0
+	
+	fmt.Println(y)
+	fmt.Println(x/y)
+
+	fmt.Println("Test123")
+
+	
+
+
+	
+
+
+}
