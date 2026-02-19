1193dc057a02696579169c1a5c88a37578f2ba10
gagan <gasingla07@gmail.com>
2026-02-19 15:29:45 -0600

    add untyped constants

--- hello/main.go
+++ hello/main.go
@@ -2,32 +2,13 @@ package main
 
 import "fmt" 
 
-var flag bool
-var isAwesome = true
+const x = 10
 
 func main() {
 
 
-	fmt.Println("hello from go")
+	var y int = x
 
-	fmt.Println("Hello from golang")
-
-	fmt.Println("Greetings and \"Salutations\"")
-
-	// fmt.Println(2.3/0.0)
-
-	var x float64 = -5.0
-	var y float64 = 0
-	
 	fmt.Println(y)
-	fmt.Println(x/y)
-
-	fmt.Println("Test123")
-
-	
-
-
-	
-
 
 }
