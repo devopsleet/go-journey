5426b5e3cb14d5f94f7d12e8e469a7ecba327d0b
gagan <gasingla07@gmail.com>
2026-03-27 09:44:27 -0500

    program to test max values

--- ch1/hello.go
+++ ch1/hello.go
@@ -1,12 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-var x int = 10
-var y float64 = 30.2
+// var x int = 10
+// var y float64 = 30.2
 
-var sum1 float64 = float64(x) + y
-var sum2 = x + int(y)
+// var sum1 float64 = float64(x) + y
+// var sum2 = x + int(y)
+
+// const a int64 = 10
+
+// // untyped constant
+// const x = 10
+
+// var y int = x
+// var z float64 = x
+// var b byte = x
+
+var b byte = math.MaxUint8
+var smallI int32 = math.MaxInt32
+var bigI uint64 = math.MaxUint64
+
 func main() {
-	fmt.Println(sum1, sum2)
+	b += 1
+	smallI += 1
+	bigI += 1
+	fmt.Println(b)
+	fmt.Println(smallI)
+	fmt.Println(bigI)
 }
