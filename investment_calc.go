a377854da3f776901b91d109dd472ff66cb193e1
gagan <gasingla07@gmail.com>
2026-03-30 14:27:09 -0500

    variable declarations

---
+++ investment_calc.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// variable declarations
+
+var x int = 10
+
+var x_without_type = 20
+
+var x_zero_val int
+
+var a,b int = 10,20
+
+var c,d = 30, "hello"
+
+
+
+func main() {
+	fmt.Println(x)
+	
+}
