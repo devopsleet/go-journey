c8eb36e926925a817fc2629db918179546ea10b2
gagan <gasingla07@gmail.com>
2026-03-31 16:49:27 -0500

    arrays

--- investment_calc.go
+++ investment_calc.go
@@ -4,17 +4,36 @@ import "fmt"
 
 // variable declarations
 
-const x = 10
+// const x = 10
+
+const a = 40
 
 
 func main() {
-	var y int = x
-	fmt.Println(y)
-	var z float64 = x
-	fmt.Println(z)
-	var d byte = x
-	fmt.Println(d)
+	 x := 10
+	 x = 20
+	 fmt.Println(x)
+	 x = 30
 
-	fmt.Print(x)
+	 _0 := 0o_1_2_3
+	 fmt.Println(_0)
+
+	 var a = [...]int{10,20,30}
+	 var b = [3]int{10,20,30}
+
+	 fmt.Println(a == b)
+
+	 var c [2][3]int
+
+	 c[0] = [3]int{1,2,3}
+	 c[0][0] = 10
+
+	 //c[0] = 10
+
+	 fmt.Println(len(c))
+
+
+
+
 	
 }
