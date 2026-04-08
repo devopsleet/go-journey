b598d883d357e6bf8ce3ac2b14115168c89aab59
gagan <gasingla07@gmail.com>
2026-04-08 13:23:24 -0500

    compilation error

--- control_structures.go
+++ control_structures.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 
-	n := rand.Intn(10)
-
-	if n == 0 {
+	if n := rand.Intn(10); n == 0 {
 		fmt.Println("This is too low")
 	} else if n > 5 {
 		fmt.Println("That's too big", n)
 	} else {
 		fmt.Println("That's a good number ", n)
 	}
+
+	fmt.Println("n is ", n)
 
 }
