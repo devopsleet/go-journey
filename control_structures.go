d503861506d3f50e9446476f50d31304f8ba5efa
gagan <gasingla07@gmail.com>
2026-04-09 09:26:39 -0500

    shadowing

--- control_structures.go
+++ control_structures.go
@@ -2,29 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	//"math/rand"
 )
 
 func main() {
 
-	if n := rand.Intn(10); n == 0 {
-		fmt.Println("This is too low")
-	} else if n > 5 {
-		fmt.Println("That's too big", n)
-	} else {
-		fmt.Println("That's a good number ", n)
+	//shadowed variable
+	x := 10
+	if x > 5 {
+		fmt.Println(x)
+		// shadowing variable
+		x := 5
+		fmt.Println(x)
 	}
-
-	evenVals := []int{2, 4, 6, 8, 10, 12}
-	for i, v := range evenVals {
-		fmt.Println(i, v)
-	}
-
-	uniqueNames := map[string]bool{"Fred": true,
-		"Rahul": true, "Wilma": true}
-
-	for k := range uniqueNames {
-		fmt.Println(k)
-	}
+	fmt.Println(x)
 
 }
