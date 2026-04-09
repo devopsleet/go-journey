a4d6642a550d7ac40a4f48ef6bee4aac8f1e63f9
gagan <gasingla07@gmail.com>
2026-04-09 09:59:31 -0500

    for like while statement

--- control_structures.go
+++ control_structures.go
@@ -2,33 +2,46 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	//"math/rand"
 )
 
 func main() {
 
-	//shadowed variable
-	x := 10
-	if x > 5 {
-		fmt.Println(x)
-		// shadowing variable
-		x := 5
-		fmt.Println(x)
+	// //shadowed variable
+	// x := 10
+	// if x > 5 {
+	// 	fmt.Println(x)
+	// 	// shadowing variable
+	// 	x := 5
+	// 	fmt.Println(x)
+	// }
+	// //fmt.Println(x)
+
+	// // shadowing language identifiers
+	// fmt.Println(true)
+	// true := 10
+	// fmt.Println(true)
+
+	// n := 15
+	// // declare variabels scoped to the condition
+	// if n := rand.Intn(10); n == 0 {
+	// 	fmt.Println("That's too low")
+	// } else if n > 5 {
+	// 	fmt.Println("That's too big:", n)
+	// } else {
+	// 	fmt.Println("That's a good number: ", n)
+	// }
+
+	// fmt.Println(n)
+
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println("i value is ", i)
+	// }
+
+	// for works like a while statement
+	j := 1
+	for j < 10 {
+		fmt.Println(j)
+		j += 2
 	}
-	fmt.Println(x)
-
-	// shadowing language identifiers
-	fmt.Println(true)
-	true := 10
-	fmt.Println(true)
-
-	// declare variabels scoped to the condition
-	if n := rand.Intn(10); n == 0 {
-		fmt.Println("That's too low")
-	} else if n > 5 {
-		fmt.Println("That's too big:", n)
-	} else {
-		fmt.Println("That's a good number: ", n)
-	}
-
 }
