cd7726ea1944e31b97e3210ffd53c4e829ee8d26
gagan <gasingla07@gmail.com>
2026-04-11 21:12:19 -0500

    more handlers added

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -12,13 +12,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+// Add a snippetView handler function
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+
+}
+
+// Add a snippetCreate handler function
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating  a new snippet ...."))
+}
+
 func main() {
 	fmt.Println("Hello World!")
 
 	// Use the http.NewServeMux() function to initialize a new serveMux, the
 	// register the home funcation as the handler for the "/" URL pattern
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Print a log message to say that the server is starting
 	log.Print("starting server on: 4000")
