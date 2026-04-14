10fb03b34c57d8a83cdc925dd6709516daf7b7cf
gagan <gasingla07@gmail.com>
2026-04-14 12:14:23 -0500

    go arrays

--- practice.go
+++ practice.go
@@ -9,13 +9,23 @@ func main() {
 	fmt.Println(r)
 	fmt.Println(s)
 
-	var (
-		x int
-		y = 20
-	)
+	arr := []int{1, 2, 3}
+	fmt.Println(arr[2])
 
-	fmt.Println("x and y", x, y)
+	// var (
+	// 	x int
+	// 	y = 20
+	// )
+
+	// fmt.Println("x and y", x, y)
 
 	// var myFirstInitial rune = "G"
 	// var mySecondInitial int32 = "F"
+
+	// arrays
+	var x = [3]int{10, 20, 30}
+	fmt.Println("The array values are ", x)
+
+	var y = [...]int{1, 2, 3, 4, 3, 3, 4, 45, 56, 5}
+	fmt.Println(y)
 }
