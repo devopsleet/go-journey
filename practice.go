134927a8d628171a9c8f5d688f215e919981b43d
gagan <gasingla07@gmail.com>
2026-04-16 14:01:21 -0500

    slicing slice

--- practice.go
+++ practice.go
@@ -23,9 +23,45 @@ func main() {
 	// var mySecondInitial int32 = "F"
 
 	// arrays
-	var x = [3]int{10, 20, 30}
-	fmt.Println("The array values are ", x)
+	// var x = [3]int{10, 20, 30}
+	// fmt.Println("The array values are ", x)
 
-	var y = [...]int{1, 2, 3, 4, 3, 3, 4, 45, 56, 5}
-	fmt.Println(y)
+	// var y = [...]int{1, 2, 3, 4, 3, 3, 4, 45, 56, 5}
+	// fmt.Println(y)
+
+	// var z = []int{10, 20, 30}
+	// fmt.Println(z)
+
+	// var a []int
+	// fmt.Println(a == nil)
+
+	// a = append(a, 1, 2)
+	// fmt.Println(a)
+
+	// var b = []int{10, 20, 30}
+	// b = append(b, 40, 50)
+
+	// a = append(a, b...)
+	// fmt.Println(a)
+
+	// fmt.Println(b)
+
+	var y []int
+	fmt.Println(y == nil)
+	fmt.Println(len(y))
+
+	z := []int{}
+	fmt.Println(z == nil)
+	fmt.Println(len(z))
+
+	x := make([]int, 5)
+	x = append(x, 10)
+	fmt.Println("Length of slice is ", len(x))
+	fmt.Println("Capacity of slice is ", cap(x))
+
+	// Emptying a Slice
+	x := []string{"a", "b", "c", "d"}
+	y = x[:2]
+	z = x[1:]
+
 }
