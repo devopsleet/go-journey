80c394608590ae317543d567b900c1656ff88be3
gagan <gasingla07@gmail.com>
2026-04-20 13:53:33 -0500

    implementation of basic calculator using functions

---
+++ calculator.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	return x / y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+var opMap = map[string]func(int, int) int{
+	"+": add,
+	"-": subtract,
+}
+
+func main() {
+	var op string
+	fmt.Println("Please enter the operator you want to use ")
+	fmt.Scan(&op)
+
+	expressions := [][]string{
+		{"2", op, "3"},
+		{"two", "+", "three"},
+	}
+
+	for _, expression := range expressions {
+		if len(expression) != 3 {
+			fmt.Println("It's an invalid expression ", expression)
+			continue
+		}
+
+		p1, err := strconv.Atoi(expression[0])
+		if err != nil {
+			fmt.Println("Error is ", err)
+			continue
+
+		}
+
+		op := expression[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("Unsupported operator ", op)
+			continue
+		}
+
+		p2, err := strconv.Atoi(expression[2])
+		if err != nil {
+			fmt.Println("The error is ", err)
+			continue
+		}
+
+		result := opFunc(p1, p2)
+		fmt.Println("The result is ", result)
+
+	}
+
+}
