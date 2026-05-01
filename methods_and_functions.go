714d17ae4ac8444af9f49ed25d7f78dffbf19a88
gagan <gasingla07@gmail.com>
2026-05-01 09:23:51 -0500

    pointer receiver

---
+++ methods_and_functions.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Counter struct {
+	counter    int
+	lastUpdate time.Time
+}
+
+func (c Counter) String() string {
+	return fmt.Sprintf("The value of counter is %d at time %s", c.counter, c.lastUpdate)
+}
+
+func (c *Counter) Increment() {
+	c.counter++
+	c.lastUpdate = time.Now()
+}
+
+func main() {
+
+	var c Counter
+	output := c.String()
+	fmt.Println(output)
+	c.Increment()
+	fmt.Println(c.String())
+
+}
