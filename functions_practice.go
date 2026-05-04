a44c4958fee3b03f7f06ee6308e00abc13dd16bb
gagan <gasingla07@gmail.com>
2026-05-04 11:09:46 -0500

    pass variadic parameters

--- functions_practice.go
+++ functions_practice.go
@@ -1,98 +1,46 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// func div(num int, denom int) int {
-// 	if denom == 0 {
-// 		return 0
-// 	}
-// 	return num / denom
-// }
-
-// type MyFuncOpts struct {
-// 	FirstName string
-// 	LastName  string
-// 	Age       int
-// }
-
-// func MyFunc(opts MyFuncOpts) error {
-
-// 	fmt.Println(opts.FirstName)
-// 	return nil
-
-// }
-
-// func addTo(base int, vals ...int) []int {
-// 	output := make([]int, 0, len(vals))
-
-// 	for _, v := range vals {
-// 		output = append(output, base+v)
-// 	}
-
-// 	return output
-// }
-
-func f1(a string) int {
-	total := 0
-	for _, v := range a {
-		total += int(v)
+func div(num int, denom int) int {
+	if denom == 0 {
+		return 0
 	}
 
-	return total
+	return num / denom
+}
+
+type MyFuncOpts struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func Myfunc(Opts MyFuncOpts) {
+
+	fmt.Printf("The data is %+v\n", Opts)
+
+}
+
+func addTo(base int, vals ...int) []int {
+	out := make([]int, 0, len(vals))
+	for _, val := range vals {
+		out = append(out, base+val)
+	}
+
+	return out
 }
 
 func main() {
 
-	var myFuncVariable func(string) int
+	fmt.Println(addTo(3, 4))
 
-	myFuncVariable = f1
+	result := div(5, 2)
+	fmt.Println("result is ", result)
 
-	result := myFuncVariable("Hello")
-	fmt.Println("The result is ", result)
-
-	// result := div(5, 2)
-	// fmt.Println(result)
-
-	// MyFunc(MyFuncOpts{
-	// 	"Gagan",
-	// 	"Singla",
-	// 	32,
-	// })
-
-	// fmt.Println(addTo(1, 3, 5, 7, 9))
-	// b := []int{2, 4, 6, 8}
-	// fmt.Println(addTo(1, b...))
-	// map declarations
-	//var m = map[string]int{}
-	// d := map[string]int{
-	// 	"Kitten": 1,
-	// }
-	// m := make(map[string]int)
-
-	// // zeo value of amp is nil
-	// if m == nil {
-	// 	fmt.Println("Map is Nil")
-	// }
-
-	// m["x"] = 1
-	// m["y"] = 2
-
-	// fmt.Println(m["z"])
-
-	// val, ok := m["z"]
-	// fmt.Println(val, ok)
-
-	// val, ok = m["world"]
-	// fmt.Println(val, ok)
-
-	// // m["a"] = 1
-
-	// var n = map[string]int{}
-
-	// n["a"] = 1
-
-	// fmt.Println(n["a"])
+	Myfunc(MyFuncOpts{
+		LastName: "Singla",
+		Age:      32,
+	})
 
 }
