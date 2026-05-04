bae3563713971a4b7b8a006a0f59fba58c9199e7
gagan <gasingla07@gmail.com>
2026-05-04 11:51:29 -0500

    use go function as value

--- functions_practice.go
+++ functions_practice.go
@@ -1,46 +1,31 @@
+// functions are values
+
 package main
 
 import "fmt"
 
-func div(num int, denom int) int {
-	if denom == 0 {
-		return 0
+var myFuncVariable func(string) int
+
+func f1(a string) int {
+	return len(a)
+}
+
+func f2(b string) int {
+	total := 0
+
+	for _, val := range b {
+		total += int(val)
 	}
 
-	return num / denom
-}
-
-type MyFuncOpts struct {
-	FirstName string
-	LastName  string
-	Age       int
-}
-
-func Myfunc(Opts MyFuncOpts) {
-
-	fmt.Printf("The data is %+v\n", Opts)
-
-}
-
-func addTo(base int, vals ...int) []int {
-	out := make([]int, 0, len(vals))
-	for _, val := range vals {
-		out = append(out, base+val)
-	}
-
-	return out
+	return total
 }
 
 func main() {
 
-	fmt.Println(addTo(3, 4))
+	myFuncVariable := f1
+	fmt.Println(myFuncVariable("Hello"))
 
-	result := div(5, 2)
-	fmt.Println("result is ", result)
-
-	Myfunc(MyFuncOpts{
-		LastName: "Singla",
-		Age:      32,
-	})
+	myFuncVariable = f2
+	fmt.Println("The sum of all the ASCII characters is ", myFuncVariable("a"))
 
 }
