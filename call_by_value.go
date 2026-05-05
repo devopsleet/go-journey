fe11fcd5bcce3568be57030a81b3712872913bf0
gagan <gasingla07@gmail.com>
2026-05-05 10:44:44 -0500

    behaviour of struct slice and map as go function parameters

--- call_by_value.go
+++ call_by_value.go
@@ -2,35 +2,46 @@ package main
 
 import "fmt"
 
-// map is not call by value
-// map is a reference type so it changes the original map
-// func modifyMap(m map[string]int) {
-// 	m["a"] = 10
-// 	m["c"] = 30
-// 	delete(m, "a")
-// }
+type person struct {
+	Name string
+	Age  int
+}
 
-func modifySlice(s []int) {
-	for idx, _ := range s {
-		s[idx] *= 2
-	}
+func modStruct(p person) {
+	p.Name = "abc"
+	fmt.Printf("%+v\n", p)
+}
 
-	s = append(s, 10)
+func modMap(m map[string]int) {
+	m["a"] = 10
+	m["c"] = 30
+	fmt.Println(m)
+
+}
+
+func modSlice(s []int) {
+	s[0] = 10
+	fmt.Println("The copied slice is ", s)
 }
 
 func main() {
 
-	// m := make(map[string]int)
-	// m["a"] = 1
-	// m["b"] = 2
-	// fmt.Println("Map before call by value ", m)
-	// modifyMap(m)
-	// fmt.Println("Map after call by value ", m)
+	p := person{
+		Name: "Gagan",
+		Age:  32,
+	}
 
-	s := make([]int, 3)
-	s = append(s, 4)
-	fmt.Println("Slice before call by value = ", s)
-	modifySlice(s)
-	fmt.Println("Slice after call by value = ", s)
+	m := map[string]int{
+		"a": 1,
+		"b": 2,
+	}
 
+	modStruct(p)
+	modMap(m)
+	fmt.Println("The original map is ", m)
+
+	s := []int{1, 2, 3, 4}
+
+	modSlice(s)
+	fmt.Println("The original slice is ", s)
 }
