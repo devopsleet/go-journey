d39004efe3665b1aeea6518061837d657276dd08
gagan <gasingla07@gmail.com>
2026-05-11 10:49:34 -0500

    call value receivers

--- go_types.go
+++ go_types.go
@@ -1,11 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 type Counter struct {
-	total int
+	total      int
+	lastUpdate time.Time
+}
+
+func (c Counter) String() string {
+	return fmt.Sprintf("The values are %d and %v", c.total, c.lastUpdate)
+}
+
+func (c *Counter) Increment() {
+
+	c.total++
+	c.lastUpdate = time.Now()
+
 }
 
 func main() {
 
+	// local variable value type
 	var c Counter
+
+	output := c.String()
+	fmt.Println(output)
 
 }
--- methods_and_functions.go
+++ methods_and_functions.go
@@ -1,30 +1,30 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"time"
+// )
 
-type Counter struct {
-	counter    int
-	lastUpdate time.Time
-}
+// type Counter struct {
+// 	counter    int
+// 	lastUpdate time.Time
+// }
 
-func (c Counter) String() string {
-	return fmt.Sprintf("The value of counter is %d at time %s", c.counter, c.lastUpdate)
-}
+// func (c Counter) String() string {
+// 	return fmt.Sprintf("The value of counter is %d at time %s", c.counter, c.lastUpdate)
+// }
 
-func (c *Counter) Increment() {
-	c.counter++
-	c.lastUpdate = time.Now()
-}
+// func (c *Counter) Increment() {
+// 	c.counter++
+// 	c.lastUpdate = time.Now()
+// }
 
-func main() {
+// func main() {
 
-	c := &Counter{}
-	output := c.String()
-	fmt.Println(output)
-	c.Increment()
-	fmt.Println(c.String())
+// 	c := &Counter{}
+// 	output := c.String()
+// 	fmt.Println(output)
+// 	c.Increment()
+// 	fmt.Println(c.String())
 
-}
+// }
