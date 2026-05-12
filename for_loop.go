9d2a39306d56b50c844c9945e4010ef5cb79173b
gagan <gasingla07@gmail.com>
2026-05-12 14:50:24 -0500

    guessing game

--- for_loop.go
+++ for_loop.go
@@ -4,34 +4,4 @@ import "fmt"
 
 func main() {
 
-	// for i := 1; i <= 10; i++ {
-	// 	if i%2 == 0 {
-	// 		continue
-	// 	}
-	// 	if i == 5 {
-	// 		break
-	// 	}
-	// 	fmt.Println("Odd number value is ", i)
-	// }
-
-	// rows := 7
-
-	// Outer loop
-	// for i := 1; i <= rows; i++ {
-	// 	// inner loop for spaces before stars
-	// 	for j := 1; j <= rows-i; j++ {
-	// 		fmt.Print(" ")
-
-	// 	}
-	// 	// inner loop for stars
-	// 	for k := 1; k <= 2*i-1; k++ {
-	// 		fmt.Print("*")
-	// 	}
-	// 	fmt.Println()
-	// }
-
-	for i := range 10 {
-		fmt.Println(i)
-	}
-
 }
---
+++ guessing_game.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.NewSource(sou)
+
+}
