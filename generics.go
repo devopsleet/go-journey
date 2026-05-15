5861c51712f28ca277c7a25358438ccca3b278a4
gagan <gasingla07@gmail.com>
2026-05-15 09:33:29 -0500

    implementation of Stack using generics

--- generics.go
+++ generics.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
-type Stack[T any] struct {
+type Stack[T comparable] struct {
 	vals []T
 }
 
 func (s *Stack[T]) Push(val T) {
+
 	s.vals = append(s.vals, val)
+
 }
 
 func (s *Stack[T]) Pop() (T, bool) {
@@ -15,21 +17,28 @@ func (s *Stack[T]) Pop() (T, bool) {
 		var zero T
 		return zero, false
 	}
-
 	top := s.vals[len(s.vals)-1]
 	s.vals = s.vals[:len(s.vals)-1]
 	return top, true
 }
 
+func (s Stack[T]) Contains(val T) bool {
+	for _, v := range s.vals {
+		if v == val {
+			return true
+		}
+	}
+
+	return false
+
+}
+
 func main() {
 
-	var intStack Stack[int]
-	intStack.Push(10)
-	intStack.Push(20)
-
-	v, ok := intStack.Pop()
-	v, ok = intStack.Pop()
-	v, ok = intStack.Pop()
-	fmt.Println(v, ok)
+	var S Stack[int]
+	S.Push(10)
+	S.Push(20)
+	output := S.Contains(20)
+	fmt.Println("The output is ", output)
 
 }
