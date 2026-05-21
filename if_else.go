5e55248407fced070bf27ed374265e34d9450300
gagan <gasingla07@gmail.com>
2026-05-21 17:00:00 -0500

    if else

---
+++ if_else.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	age := 25
+	if age >= 18 {
+		fmt.Println("You are an adult. ")
+	} else {
+
+	}
+
+	temperature := 25
+
+	if temperature > 30 {
+		fmt.Println("It's hot")
+	} else {
+		fmt.Println("Cool")
+	}
+
+	score := 
+}
