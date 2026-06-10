56213d9625f114f29280a2984f8b02aedac53e2b
gagan <gasingla07@gmail.com>
2026-06-10 11:03:53 -0500

    go error handling

---
+++ go_error_handling.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"time"
+)
+
+var ranOnce bool
+
+var (
+	ErrNetwork = errors.New("network error")
+	ErrInput   = errors.New("input error")
+)
+
+func someFunc(data string) error {
+	if !ranOnce {
+		ranOnce = true
+		return ErrNetwork
+	}
+
+	return ErrInput
+}
+
+func main() {
+
+	for {
+		err := someFunc("data")
+		if err == nil {
+			break
+		}
+
+		if errors.Is(err, ErrNetwork) {
+			log.Println("recoverable network error")
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		log.Println("unrecoverable error")
+		break
+	}
+}
