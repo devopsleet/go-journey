63d63bff87b76bf6236218d08d01910ae2395a9a
gagan <gasingla07@gmail.com>
2026-06-18 12:49:58 -0500

    basic error handling in go

--- generics.go
+++ generics.go
@@ -2,43 +2,31 @@ package main
 
 import "fmt"
 
-type Stack[T comparable] struct {
+type Stack[T any] struct {
 	vals []T
 }
 
 func (s *Stack[T]) Push(val T) {
-
 	s.vals = append(s.vals, val)
-
 }
 
 func (s *Stack[T]) Pop() (T, bool) {
 	if len(s.vals) == 0 {
 		var zero T
 		return zero, false
+
 	}
+
 	top := s.vals[len(s.vals)-1]
 	s.vals = s.vals[:len(s.vals)-1]
+
 	return top, true
 }
-
-func (s Stack[T]) Contains(val T) bool {
-	for _, v := range s.vals {
-		if v == val {
-			return true
-		}
-	}
-
-	return false
-
-}
-
 func main() {
 
-	var S Stack[int]
-	S.Push(10)
-	S.Push(20)
-	output := S.Contains(20)
-	fmt.Println("The output is ", output)
+	var intStack Stack[int]
+	intStack.Push(10)
+	v, ok := intStack.Pop()
+	fmt.Println(v, ok)
 
 }
