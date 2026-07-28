7989d971a92d1e88fc2b25f4298f6614f20f7877
gagan <gasingla07@gmail.com>
2026-07-28 14:34:06 -0500

    data types

---
+++ basics/data_types.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var age int
+
+	var name = "John"
+
+	count := 10
+
+	lastName = "Singh"
+}
+
+// Default values
+
+// Numeric Types: 0
+// Boolean Types: false
+// String Type : ""
+// Pointers, Slices, maps, functions, and structs: nil
+
+// --- Scope
+func printName() {
+	firstName := "gagan"
+}
