1dafd49da27d37c50ad4fab731603cdb662d5fd5
gagan <gasingla07@gmail.com>
2026-07-29 13:18:35 -0500

    explicit type conversion

---
+++ ch1/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var myFirstInital rune = 'G'
+var myLastInitial int32 = 'S'
+
+func main() {
+
+	var x int = 10
+	var y float64 = 30.2
+	var sum1 float64 = float64(x) + y
+
+	fmt.Println("Total sum is", sum1)
+	//fmt.Println("Hello World")
+
+}
