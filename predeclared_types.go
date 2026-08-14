71c373d05f42bb545b0a23ed89ae69777b7d69a7
gagan <gasingla07@gmail.com>
2026-08-14 11:15:04 -0500

    predeclared types

---
+++ predeclared_types.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var r rune = 'a'
+	fmt.Println("Rune literal value is ", r)
+	fmt.Printf("Character value is %c\n", r)
+
+	str := "Greetings and\n\"Salutations"
+	fmt.Printf("The value of a string is %s", str)
+
+	var flag bool
+	fmt.Println("The default value of flag is", flag)
+
+	var isAwesome = true
+	fmt.Println("The value of isAwesome is ", isAwesome)
+
+	var myFirstInitial rune = 'G'
+	var myLastinitial int32 = 'S'
+
+	fmt.Printf("The first initial is %c and last initail is %c", myFirstInitial, myLastinitial)
+}
