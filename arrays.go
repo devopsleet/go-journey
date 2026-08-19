b24f12a058e4427bcdeccfd34094b01ee280f5be
gagan <gasingla07@gmail.com>
2026-08-19 13:52:31 -0500

    slices Equal method

--- arrays.go
+++ arrays.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 var x = [3]int{10, 20, 30}
 var y = [12]int{1, 5: 4, 6, 10: 100, 12}
 var z = [...]int{10, 20, 30}
+
+var a = []int{1, 2, 3}
+var b = []int{1, 2, 3}
+var c = []int{1, 2, 3, 4}
+var d = []string{"a", "b", "c"}
 
 func main() {
 	fmt.Println(x == z)
@@ -16,4 +24,8 @@ func main() {
 
 	var s []int
 	fmt.Println(s == nil)
+
+	fmt.Println(slices.Equal(a, b))
+	fmt.Println(slices.Equal(b, c))
+	//fmt.Println(slices.Equal(c, d))
 }
